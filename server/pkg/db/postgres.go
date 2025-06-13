1be448789ee2f629857740ab35551af060d04f3e
HugManh <manh2909@gmail.com>
2025-06-13 11:11:15 +0700

    feat: connect postgres

--- server/pkg/db/postgres.go
+++ server/pkg/db/postgres.go
@@ -2,28 +2,30 @@ package db
 
 import (
 	"fmt"
-	"os"
+	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
-
-func Init() {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_SSLMODE"),
-	)
-
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect to database: " + err.Error())
+func Init(dbhost string, dbport int, dbuser, dbpass, dbname string, usessl bool) *gorm.DB {
+	dbssl := "disable"
+	if usessl {
+		dbssl = "require"
 	}
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		dbhost, dbport, dbuser, dbpass, dbname, dbssl,
+	)
+	log.Printf("Connecting to database with DSN: %s", dsn)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// if err != nil {
+	// 	panic("failed to connect to database: " + err.Error())
+	// }
+
+	return db
 }
