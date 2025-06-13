275b796f44e06e4840622c199c3e0a3279ea1453
HugManh <manh2909@gmail.com>
2025-06-13 11:45:51 +0700

    feat: edit user model

--- server/internal/user/model/user.go
+++ server/internal/user/model/user.go
@@ -5,5 +5,7 @@ type User struct {
 	Name     string `gorm:"type:varchar(100)"`
 	Username string `gorm:"type:varchar(100)"`
 	Email    string `gorm:"uniqueIndex"`
-	Password string
+	Role     string `gorm:"type:varchar(50);default:'user'"` // e.g., 'admin', 'user', etc.
+	Password string `gorm:"type:varchar(255)"`               // Store hashed password
+	IsActive bool   `gorm:"default:true"`
 }
