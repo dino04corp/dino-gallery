0ec6c1f3835cf80bef3fd3f9d747df02886b9825
HugManh <manh2909@gmail.com>
2025-07-02 17:45:13 +0700

    feat: upload, resource model

--- server/internal/upload/model/upload.go
+++ server/internal/upload/model/upload.go
@@ -2,13 +2,52 @@ package model
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
+const (
+	UploadPending = "pending"
+	UploadSuccess = "success"
+	UploadFailed  = "failed"
+
+	ResourceActive   = "active"
+	ResourceArchived = "archived"
+	ResourceDeleted  = "deleted"
+)
+
+type BaseModel struct {
+	ID        uint           `gorm:"primaryKey;autoIncrement" json:"id"`
+	CreatedAt time.Time      `gorm:"column:created_at;autoCreateTime;<-:create" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at;autoUpdateTime" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at;index" json:"-"`
+}
+
 type Upload struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name" gorm:"not null"`
-	Size      int64     `json:"size" gorm:"not null"`
-	Type      string    `json:"type" gorm:"not null"`
-	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	BaseModel
+
+	// UserID *uint `gorm:"index" json:"user_id,omitempty"` // nếu có đăng nhập
+	// User   *User `gorm:"foreignKey:UserID" json:"-"`
+
+	Filename  string  `gorm:"type:varchar(255);not null" json:"filename"`       // tên file user cung cấp
+	Status    string  `gorm:"type:varchar(50);default:'pending'" json:"status"` // pending, success, failed
+	IPAddress string  `gorm:"type:varchar(45);" json:"ip_address"`              // optional
+	ErrorMsg  *string `gorm:"type:text" json:"error_msg,omitempty"`             // nếu upload lỗi
+	Source    *string `gorm:"type:varchar(255);" json:"source,omitempty"`       // UI, API, SDK...
+
+	ResourceID *uint     `gorm:"index" json:"resource_id,omitempty"` // nếu upload thành công
+	Resource   *Resource `gorm:"foreignKey:ResourceID" json:"resource,omitempty"`
+}
+
+type Resource struct {
+	BaseModel
+
+	Name       string `gorm:"type:varchar(255);not null;index" json:"name"`
+	Type       string `gorm:"type:varchar(100);not null;index" json:"type"`         // MIME type
+	Size       int64  `gorm:"not null" json:"size"`                                 // Byte size
+	Hash       string `gorm:"type:char(64);uniqueIndex" json:"hash"`                // File hash (SHA256/MD5) để deduplicate
+	Path       string `gorm:"type:varchar(512);not null" json:"path"`               // Đường dẫn trong storage (VD: S3 key)
+	Storage    string `gorm:"type:varchar(50);default:'local'" json:"storage"`      // local, s3, gcs...
+	Status     string `gorm:"type:varchar(50);default:'active'" json:"status"`      // active, deleted, archived
+	Visibility string `gorm:"type:varchar(20);default:'private'" json:"visibility"` // public, private, protected
 }
